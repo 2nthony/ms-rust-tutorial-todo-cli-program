3693efa7379ecce967104e1d96a2e8ebf77e1169
2nthony <ant.hu@outlook.com>
2022-04-04 13:09:08 +0800

    feat: finish

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
+use anyhow::anyhow;
 mod cli;
 mod tasks;
-use structopt::StructOpt;
-
 use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
+use structopt::StructOpt;
 use tasks::Task;
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // println!("{:#?}", cli::CommandLineArgs::from_args());
     let CommandLineArgs {
         action,
         journal_file,
     } = CommandLineArgs::from_args();
 
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file");
 
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+    // .expect("Failed to perform action")
+
+    Ok(())
+}
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    let journal_file_name = ".rust-todo-journal.json";
+
+    home::home_dir().map(|mut path| {
+        path.push(journal_file_name);
+        path
+    })
 }
